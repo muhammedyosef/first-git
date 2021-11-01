e90a7ae5c660892ad2a3ef0998f36b18d27cc6eb
mohamed salah abdellatif <m.salah1595@gmail.com>
2021-11-01 16:42:40 +0200

    MohamedSalah commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ProductComponent } from './hamada/product/product.component';
+import { SalahComponent } from './hamada/salah/salah.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    SalahComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/hamada/salah/salah.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { SalahComponent } from './salah.component';
+
+describe('SalahComponent', () => {
+  let component: SalahComponent;
+  let fixture: ComponentFixture<SalahComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ SalahComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(SalahComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
